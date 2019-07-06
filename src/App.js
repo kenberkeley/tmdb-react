ebb774fa3cab4d545a00cac2545a42cb80eb6fa7
kenberkeley <kenberkeley@foxmail.com>
2019-07-06 22:28:40 +1000

    Introduce react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+
+import Home from './pages/home/'
+import Watchlist from './pages/watchlist/'
 
 const App = () => (
-  <div>
-    app
-  </div>
+  <Router>
+    <div>
+      <nav>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/watchlist'>Watch List</Link>
+        </li>
+      </nav>
+    </div>
+    <Route path='/' exact component={Home} />
+    <Route path='/watchlist' component={Watchlist} />
+  </Router>
 )
 
 export default App
---
+++ src/pages/watchlist/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Watchlist = () => (
+  <h1>
+    Watchlist
+  </h1>
+)
+
+export default Watchlist
