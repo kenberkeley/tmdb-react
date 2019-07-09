8dd22ce897a51e7ad8cd3932f28454caaddf67b2
kenberkeley <kenberkeley@foxmail.com>
2019-07-09 14:13:42 +1000

    Deploy to Github Pages (thanks to https://bit.ly/30rSgdV)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { HashRouter as Router, Route } from 'react-router-dom'
 
 import Layout from './layouts/default/'
 import routes from './pages/'
 
 const App = () => (
-  <Router>
+  <Router basename={process.env.PUBLIC_URL}>
     <Helmet titleTemplate='%s - My TV Shows'>
       <meta name='description' content='A TMDb demo powered by React.js' />
     </Helmet>
